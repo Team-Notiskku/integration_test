37d07ecbb8242dbdb37d0352e9bb788a4f51b9c7
i-chaeyeon <leechae0605@naver.com>
2025-03-19 11:30:33 +0900

    feat: open url

--- integration_test/lib/main.dart
+++ integration_test/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'google_sheets_api.dart';
 
 void main() {
@@ -21,8 +22,8 @@ class GoogleSheetsPage extends StatefulWidget {
 
 class _GoogleSheetsPageState extends State<GoogleSheetsPage> {
   List<List<String>> sheetData = [];
-  int currentRow = 1; 
-  bool isLoading = false; 
+  int currentRow = 1;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -43,6 +44,21 @@ class _GoogleSheetsPageState extends State<GoogleSheetsPage> {
     });
   }
 
+  void _launchURL(String url) async {
+  final Uri uri = Uri.parse(url);
+  
+  if (await canLaunchUrl(uri)) {
+    bool launched = await launchUrl(uri, mode: LaunchMode.externalApplication);
+    if (launched) {
+      print("URL Opened");
+    } else {
+      print("CANNOT OPEN ERROR: $url");
+    }
+  } else {
+    print("INVALID ERROR: $url");
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,8 +77,24 @@ class _GoogleSheetsPageState extends State<GoogleSheetsPage> {
                           child: Text("더 보기"),
                         );
                 }
+
+                String noticeText = sheetData[index].sublist(0, sheetData[index].length - 1).join(" | "); 
+                String url = sheetData[index].last; 
+
                 return ListTile(
-                  title: Text(sheetData[index].join(" | ")), 
+                  title: Text(
+                    noticeText,
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  onTap: () {
+                    if (Uri.parse(url).isAbsolute) {
+                      _launchURL(url);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('잘못된 URL 형식: $url')),
+                      );
+                    }
+                  },
                 );
               },
             ),
